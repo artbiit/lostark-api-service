a7a7e4e062b989a1f9c57405452ed5a9201fffd9
artbiit <artbiit@naver.com>
2025-08-20 17:25:24 +0900

    [Fix] 목표에 맞게 설정 수정

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,9 +1,23 @@
 module.exports = {
-    semi: true,
-    singleQuote: true,
-    trailingComma: 'all',
-    printWidth: 100,
-    tabWidth: 2,
-    endOfLine: 'lf'
-  };
-  
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  overrides: [
+    {
+      files: '*.json',
+      options: {
+        printWidth: 120,
+      },
+    },
+    {
+      files: '*.md',
+      options: {
+        printWidth: 80,
+        proseWrap: 'always',
+      },
+    },
+  ],
+};
